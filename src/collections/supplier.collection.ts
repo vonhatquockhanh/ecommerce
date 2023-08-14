15f11289147a8e42bad66b1e22dfc88c4f9ed76a
phatnguyencarhay <phat@carhay.vn>
2023-08-14 16:51:54 +0700

    Supplier logo

--- src/collections/supplier.collection.ts
+++ src/collections/supplier.collection.ts
@@ -1,6 +1,7 @@
 import { isAdmin } from '../access/admins';
 import { isSupplier } from '../access/supplier';
 import { CollectionConfig } from '../payload/collections/config/types';
+import { MediaCollection } from './media.collection';
 
 export const SupplierCollection: CollectionConfig = {
   slug: 'supplier',
@@ -10,6 +11,12 @@ export const SupplierCollection: CollectionConfig = {
   },
   fields: [
     { name: 'supplier_name', label: 'Supplier name', type: 'text', required: true },
+    {
+      name: 'supplier_login',
+      label: 'Supplier Logo',
+      type: 'upload',
+      relationTo: MediaCollection.slug,
+    },
     { name: 'supplier_description', label: 'Supplier Description', type: 'textarea', required: false },
   ],
   access: {
