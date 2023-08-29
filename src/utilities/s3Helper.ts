e2ac5b373f02f5f07588237147f0fd5733ee132e
tuanle <vonhatquockhanh@gmail.com>
2023-08-29 11:45:17 +0700

    update key s3

--- src/utilities/s3Helper.ts
+++ src/utilities/s3Helper.ts
@@ -7,6 +7,10 @@ export const buildAdapter = () => {
         process?.env?.S3_DOMAIN
       }`,
       region: process?.env?.S3_REGION,
+      credentials: {
+        accessKeyId: process?.env?.S3_ACCESS_KEY,
+        secretAccessKey: process?.env?.S3_SECRET_KEY,
+      },
     },
     bucket: process?.env?.S3_BUCKET,
   });
