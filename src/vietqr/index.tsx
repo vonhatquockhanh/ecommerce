21dafdb646b54d2552640e54a276aa4f511267a4
phatnguyencarhay <phat@carhay.vn>
2023-08-30 15:50:29 +0700

    Update viet qr client key

--- src/vietqr/index.tsx
+++ src/vietqr/index.tsx
@@ -1,8 +1,8 @@
 import { VietQR } from 'vietqr';
 
 const vietQR = new VietQR({
-    clientID: 'de8a0804-a76d-41e5-8ad6-31503ce7d5f4',
-    apiKey: '17c29f09-4ea2-4417-b9c2-7f020d35de42',
+    clientID: '5303dab7-f893-4221-87e0-25d783c9551e',
+    apiKey: 'cfbea72d-136b-4f15-ac21-e551d0a48087',
 });
 
 export default vietQR;
